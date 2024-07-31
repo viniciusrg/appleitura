1ec64fc3243d0ab7a5c2b9522b4747363fdfb73e
Vinícius <viniciusrg10@msn.com>
2024-07-30 22:55:03 -0300

    test: webhook log

--- app/Http/Controllers/WebhookController.php
+++ app/Http/Controllers/WebhookController.php
@@ -3,11 +3,13 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Log;
 
 class WebhookController extends Controller
 {
     public function handle(Request $request)
     {
-        dd($request->all());
+        Log::info('Webhook received:', $request->all());
+        // dd($request->all());
     }
 }
