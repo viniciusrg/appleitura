0e1406bf4a51bc92cd37dc59c8a0e9185305d1a4
Vinícius <viniciusrg10@msn.com>
2024-08-13 18:37:10 -0300

    fix: max_size upload

--- app/Http/Requests/StoreBookRequest.php
+++ app/Http/Requests/StoreBookRequest.php
@@ -27,7 +27,7 @@ class StoreBookRequest extends FormRequest
             'description' => 'required|string|max:1000',
             'author' => 'required|string|max:255',
             'read_time' => 'required|string|max:255',
-            'content_audio' => 'nullable|file|mimes:mp3,wav,m4a|max:256000',
+            'content_audio' => 'nullable|file|mimes:mp3,wav,m4a|max:524.288',
             'cover' => 'nullable|file|image|max:5120',
             'categories_id' => 'nullable',
         ];
