198913b785188f28ccf9c933b5367918403e494f
Vinícius <viniciusrg10@msn.com>
2024-08-13 20:42:29 -0300

    fix: max_file

--- app/Http/Requests/StoreBookRequest.php
+++ app/Http/Requests/StoreBookRequest.php
@@ -27,7 +27,7 @@ class StoreBookRequest extends FormRequest
             'description' => 'required|string|max:1000',
             'author' => 'required|string|max:255',
             'read_time' => 'required|string|max:255',
-            'content_audio' => 'nullable|file|mimes:mp3,wav,m4a|max:524.288',
+            'content_audio' => 'nullable|file|mimes:mp3,wav,m4a|max:524288',
             'cover' => 'nullable|file|image|max:5120',
             'categories_id' => 'nullable',
         ];
