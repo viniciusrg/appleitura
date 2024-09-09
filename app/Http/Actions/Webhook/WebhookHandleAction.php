6fb20fee90829d23c8e17d79d98744ecc6ba5a9f
Vinícius <viniciusrg10@msn.com>
2024-09-09 10:12:44 -0300

    test: webhook log.

--- app/Http/Actions/Webhook/WebhookHandleAction.php
+++ app/Http/Actions/Webhook/WebhookHandleAction.php
@@ -11,6 +11,7 @@ class WebhookHandleAction
     public function execute($request)
     {
         try {
+            Log::info('Webhook recebido:', ['payload' => $request->all()]);
             $category = Category::where('name', $request->Product['product_name'])->firstOrFail();
             $user = User::where('email', $request->Customer['email'])->firstOrFail();
 
