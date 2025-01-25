4045da32866981593b625031729a1bc13540c159
Vinícius <viniciusrg10@msn.com>
2025-01-25 14:59:00 -0300

    debug

--- app/Http/Requests/StoreUpdateUserRequest.php
+++ app/Http/Requests/StoreUpdateUserRequest.php
@@ -8,21 +8,14 @@ use Illuminate\Validation\Rule;
 
 class StoreUpdateUserRequest extends FormRequest
 {
-    /**
-     * Determine if the user is authorized to make this request.
-     */
     public function authorize(): bool
     {
         return true;
     }
 
-    /**
-     * Get the validation rules that apply to the request.
-     *
-     * @return array<string, \Illuminate\Contracts\Validation\ValidationRule|array<mixed>|string>
-     */
     public function rules(): array
     {
+        dump('Entrei no rules');
         try {
             $rules = [
                 'email' => [
