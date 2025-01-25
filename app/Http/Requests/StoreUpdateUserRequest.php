14708b967c30c2a020d4a3a3019ea1807ee49988
Vinícius <viniciusrg10@msn.com>
2025-01-25 14:49:46 -0300

    debug

--- app/Http/Requests/StoreUpdateUserRequest.php
+++ app/Http/Requests/StoreUpdateUserRequest.php
@@ -3,6 +3,7 @@
 namespace App\Http\Requests;
 
 use Illuminate\Foundation\Http\FormRequest;
+use Illuminate\Support\Facades\Log;
 use Illuminate\Validation\Rule;
 
 class StoreUpdateUserRequest extends FormRequest
@@ -22,35 +23,39 @@ class StoreUpdateUserRequest extends FormRequest
      */
     public function rules(): array
     {
-        $rules = [
-            'email' => [
-                'required',
-                'email',
-                'max:255',
-                "unique:users"
-            ],
-            'password' => [
-                'required',
-                'min:6',
-                'max:100',
-            ],
-        ];
-
-        if ($this->method() === 'PATCH') {
-            $rules['email'] = [
-                'required',
-                'email',
-                'max:255',
-                Rule::unique('users')->ignore($this->id),
+        try {
+            $rules = [
+                'email' => [
+                    'required',
+                    'email',
+                    'max:255',
+                    "unique:users"
+                ],
+                'password' => [
+                    'required',
+                    'min:6',
+                    'max:100',
+                ],
             ];
 
-            $rules['password'] = [
-                'nullable',
-                'min:6',
-                'max:100',
-            ];
+            if ($this->method() === 'PATCH') {
+                $rules['email'] = [
+                    'required',
+                    'email',
+                    'max:255',
+                    Rule::unique('users')->ignore($this->id),
+                ];
+
+                $rules['password'] = [
+                    'nullable',
+                    'min:6',
+                    'max:100',
+                ];
+            }
+
+            return $rules;
+        } catch (\Exception $e) {
+            Log::error(['User register error: ' . $e]);
         }
-
-        return $rules;
     }
 }
