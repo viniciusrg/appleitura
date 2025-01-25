84bfbcce3cfcdc43d343c92ad526d0c76d4dfa22
Vinícius <viniciusrg10@msn.com>
2025-01-25 15:02:20 -0300

    debug

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -15,7 +15,7 @@ use Illuminate\Support\Facades\Log;
 
 class AuthController extends Controller
 {
-    public function register(StoreUpdateUserRequest $request)
+    public function register(Request $request)
     {
         dd('cu');
         try {
