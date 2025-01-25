4e8bac569df7a6c41bb3f1fa173ae8f5c8a81921
Vinícius <viniciusrg10@msn.com>
2025-01-25 15:06:53 -0300

    debug

--- routes/api.php
+++ routes/api.php
@@ -15,7 +15,7 @@ use App\Http\Controllers\WebhookController;
 use Illuminate\Support\Facades\Route;
 
 // Public routes
-Route::post('/register', [AuthController::class, 'register']);
+Route::post('/register2', [AuthController::class, 'register']);
 Route::post('/login', [AuthController::class, 'login']);
 Route::post('/forgot-password', [AuthController::class, 'forgotPassword']);
 Route::patch('/reset-password', [AuthController::class, 'resetPassword']);
