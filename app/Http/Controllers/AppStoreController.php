f7d46791b80164599a32fb685cb0c54b099cc87e
Vinícius <viniciusrg10@msn.com>
2025-02-01 14:27:12 -0300

    chore: recebe notification

--- app/Http/Controllers/AppStoreController.php
+++ app/Http/Controllers/AppStoreController.php
@@ -58,7 +58,7 @@ class AppStoreController extends Controller
         $data = $request->all();
         $header = $request->headers;
         $signedTransactionInfo = [];
-        Log::info('Headers', $header);
+        Log::info('Headers', [$header]);
         Log::info('Recebido Apple Notification', $data);
 
         $parts = explode('.', $data['signedPayload']);
@@ -102,11 +102,17 @@ class AppStoreController extends Controller
 
 
         // Processar a notificação conforme o tipo
-        $notificationType = $data['notificationType'] ?? null;
+        $notificationType = $signedTransactionInfo[1]->transactionReason ?? null;
         $payload = $data['data'] ?? [];
 
+        // dump("data:");
+        // dump($data);
+
+        dump("NotificationType");
+        dump($notificationType);
+
         switch ($notificationType) {
-            case 'SUBSCRIBED':
+            case ('SUBSCRIBED' || 'PURCHASE'):
                 $this->processSubscribed($signedTransactionInfo, $signedRenewalInfo);
                 break;
 
@@ -125,9 +131,15 @@ class AppStoreController extends Controller
         return response()->json(['status' => 'success'], 200);
     }
 
+    public function transactionStore(Request $request)
+    {
+        $trasaction = new StoreAppleTransactionAction();
+        return $trasaction->execute($request);
+    }
+
     private function processSubscribed(array $signedTransactionInfo, array $signedRenewalInfo)
     {
-
+        Log::info('Assinatura comprada', [$signedTransactionInfo, $signedRenewalInfo]);
     }
 
     private function processRenewal(array $signedTransactionInfo, array $signedRenewalInfo)
@@ -137,45 +149,7 @@ class AppStoreController extends Controller
 
     private function processCancel(array $signedTransactionInfo, array $signedRenewalInfo)
     {
-
+        Log::info('Assinatura cancelada', [$signedTransactionInfo, $signedRenewalInfo]);
     }
 
-    public function transactionStore(Request $request)
-    {
-        $trasaction = new StoreAppleTransactionAction();
-        return $trasaction->execute($request);
-    }
-
-    private function decodToken($response)
-    {
-        $publicKey = "-----BEGIN CERTIFICATE-----
-MIIEMDCCA7agAwIBAgIQfTlfd0fNvFWvzC1YIANsXjAKBggqhkjOPQQDAzB1MUQw
-QgYDVQQDDDtBcHBsZSBXb3JsZHdpZGUgRGV2ZWxvcGVyIFJlbGF0aW9ucyBDZXJ0
-aWZpY2F0aW9uIEF1dGhvcml0eTELMAkGA1UECwwCRzYxEzARBgNVBAoMCkFwcGxl
-IEluYy4xCzAJBgNVBAYTAlVTMB4XDTIzMDkxMjE5NTE1M1oXDTI1MTAxMTE5NTE1
-MlowgZIxQDA+BgNVBAMMN1Byb2QgRUNDIE1hYyBBcHAgU3RvcmUgYW5kIGlUdW5l
-cyBTdG9yZSBSZWNlaXB0IFNpZ25pbmcxLDAqBgNVBAsMI0FwcGxlIFdvcmxkd2lk
-ZSBEZXZlbG9wZXIgUmVsYXRpb25zMRMwEQYDVQQKDApBcHBsZSBJbmMuMQswCQYD
-VQQGEwJVUzBZMBMGByqGSM49AgEGCCqGSM49AwEHA0IABEFEYe/JqTqyQv/dtXka
-uDHCScV129FYRV/0xiB24nCQkzQf3asHJONR5r0RA0aLvJ432hy1SZMouvyfpm26
-jXSjggIIMIICBDAMBgNVHRMBAf8EAjAAMB8GA1UdIwQYMBaAFD8vlCNR01DJmig9
-7bB85c+lkGKZMHAGCCsGAQUFBwEBBGQwYjAtBggrBgEFBQcwAoYhaHR0cDovL2Nl
-cnRzLmFwcGxlLmNvbS93d2RyZzYuZGVyMDEGCCsGAQUFBzABhiVodHRwOi8vb2Nz
-cC5hcHBsZS5jb20vb2NzcDAzLXd3ZHJnNjAyMIIBHgYDVR0gBIIBFTCCAREwggEN
-BgoqhkiG92NkBQYBMIH+MIHDBggrBgEFBQcCAjCBtgyBs1JlbGlhbmNlIG9uIHRo
-aXMgY2VydGlmaWNhdGUgYnkgYW55IHBhcnR5IGFzc3VtZXMgYWNjZXB0YW5jZSBv
-ZiB0aGUgdGhlbiBhcHBsaWNhYmxlIHN0YW5kYXJkIHRlcm1zIGFuZCBjb25kaXRp
-b25zIG9mIHVzZSwgY2VydGlmaWNhdGUgcG9saWN5IGFuZCBjZXJ0aWZpY2F0aW9u
-IHByYWN0aWNlIHN0YXRlbWVudHMuMDYGCCsGAQUFBwIBFipodHRwOi8vd3d3LmFw
-cGxlLmNvbS9jZXJ0aWZpY2F0ZWF1dGhvcml0eS8wHQYDVR0OBBYEFAMs8Pjs6VhW
-GQlzE2ZOE+GX4Oo/MA4GA1UdDwEB/wQEAwIHgDAQBgoqhkiG92NkBgsBBAIFADAK
-BggqhkjOPQQDAwNoADBlAjEA8yRNdskp506DFdPLghLLJwAv5J8hBGLaI8DExdcP
-X+aBKjjO8eUo9KpfpcNYUY5YAjAPXmMXEZL+Q02adrmmshNxz3NnKm+ouQwU7vBT
-n0LvlM7vps2YslVTamRYL4aSs5k=
------END CERTIFICATE-----";
-
-        $decoded = JWT::decode($response, new key($publicKey, 'HS256'));
-        $decodedArray = (array) $decoded;
-        return $decodedArray;
-    }
 }
