f72b0942ea088f4c3ce316ca7d032a6bcc497abb
Vinícius <viniciusrg10@msn.com>
2025-02-13 21:51:05 -0300

    chore: ajusta seeder.

--- database/seeders/CategoriesSeeder.php
+++ database/seeders/CategoriesSeeder.php
@@ -10,28 +10,26 @@ class CategoriesSeeder extends Seeder
     public function run(): void
     {
         $categories = [
-            'Vida Positiva',
-            'Futuro Financeiro',
-            'Cuidar & Crescer',
-            'Conexão Íntima',
-            'Evolução Diária',
-            'Gratuito'
+            ['name' => 'Vida Positiva', 'productId' => 'vida_positiva_mensal'],
+            ['name' => 'Vida Positiva', 'productId' => 'vida_positiva_trimestral'],
+            ['name' => 'Vida Positiva', 'productId' => 'vida_positiva_anual'],
+            ['name' => 'Futuro Financeiro', 'productId' => 'futuro_financeiro_mensal'],
+            ['name' => 'Futuro Financeiro', 'productId' => 'futuro_financeiro_trimestral'],
+            ['name' => 'Futuro Financeiro', 'productId' => 'futuro_financeiro_anual'],
+            ['name' => 'Cuidar & Crescer', 'productId' => 'cuidar_e_crescer_mensal'],
+            ['name' => 'Cuidar & Crescer', 'productId' => 'cuidar_e_crescer_trimestral'],
+            ['name' => 'Cuidar & Crescer', 'productId' => 'cuidar_e_crescer_anual'],
+            ['name' => 'Conexão Íntima', 'productId' => 'conexao_intima_mensal'],
+            ['name' => 'Conexão Íntima', 'productId' => 'conexao_intima_trimestral'],
+            ['name' => 'Conexão Íntima', 'productId' => 'conexao_intima_anual'],
+            ['name' => 'Evolução Diária', 'productId' => 'evolucao_diaria_mensal'],
+            ['name' => 'Evolução Diária', 'productId' => 'evolucao_diaria_trimestral'],
+            ['name' => 'Evolução Diária', 'productId' => 'evolucao_diaria_anual'],
+            ['name' => 'Gratuito', 'productId' => 'gratuito'],
         ];
 
-        $productId = [
-            'com.app.livall',
-            'teste',
-            'teste',
-            'teste',
-            'teste',
-            'teste',
-        ];
-
-        foreach ($categories as $index => $category) {
-            Category::create([
-                'name' => $category,
-                'productId' => $productId[$index], // Associa o productId correspondente
-            ]);
+        foreach ($categories as $categorie) {
+            Category::create($categorie);
         }
     }
 }
