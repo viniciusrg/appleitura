a1735dde9087b00d36cc8517527d336502fbdab7
Vinícius <viniciusrg10@msn.com>
2025-02-22 09:24:23 -0300

    fix: ajusta UserCategoryService

--- app/Services/UserCategoryServices.php
+++ app/Services/UserCategoryServices.php
@@ -8,6 +8,6 @@ class UserCategoryServices
 {
     public static function getCategoryIds(User $user)
     {
-        return $user->categories->pluck('id')->toArray();
+        return $user->subcategories->pluck('id')->toArray();
     }
 }
